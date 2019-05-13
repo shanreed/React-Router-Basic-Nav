95d2a4feb8bbeba579118c93614291de18662716
shanreed <srtech5set@gmail.com>
2019-05-13 12:09:07 -0700

    added the links for navigation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 const App = () => (
   <div>
     <Navigation />
     <Route exact path="/" component={Home} />
-    <Route exact path="/about" component={About} />
+    <Route path="/about" component={About} />
     <Route path="/contact" component={Contact} />
   </div>
 );
